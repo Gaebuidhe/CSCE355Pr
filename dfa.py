9e6fa1324a868cdffed14fe4d38832abc70393e3
Joe Frazier <faolannus@gmail.com>
2018-11-01 12:55:47 -0400

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -3,24 +3,45 @@ import sys
 import time
 
 class Dfa:
-    #initiates dfa with blank states and accepting states
+    # initiates dfa with blank states and accepting states
     def __init__(self):
         statesNo = None
         global accepting
         accepting = []
         global states
         states = []
+        global alphabet
 
-    #
+    # setter function to get info from the user for the dfa
     def set_dfa(self):
-        print("Number of States:")
-        statesNo = int(input())
-        print("Accepting states:")
-        newAccepting = raw_input()
+        # First get the number of states in the DFA
+        statesNo = raw_input("Number of States: ")
+
+        # Checks if input is integer if not re-asks for integer
+        while statesNo.isdigit()==False:
+            print("Please input integer...")
+            statesNo = raw_input()
+
+        # Next asks for accepting states
+        newAccepting = raw_input("Accepting states: ")
+
+        # Checks if accepting states are all
+        while self.check_input(newAccepting) == False:
+            print("Improper Accepting states, ensure states are integers.")
+            newAccepting = raw_input("Accepting states: ")
+
         mapping = newAccepting.split()
         accepting = map(int, mapping)
-        for i in range(0,statesNo):
+        for i in range(0,int(statesNo)):
             if i == 0:
                 states.append(raw_input("Input first state: "))
             else:
                 states.append(raw_input("Input next state: "))
+
+    # checks if input is a possible array of
+    def check_input(chkString):
+        foo = chkString.split()
+        for i in range(0,len(foo)):
+            if foo[i].isdigit == False:
+                return False
+        return True
